faaad4eb877d5097c0448c38d4dcf21bc94cbdc4
deepak-delta <ddk3857@gmail.com>
2022-10-22 22:43:14 +0530

    Added CORS

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ import { boolean } from 'zod'
 import deserializeUser from './middelware/deserializeUser'
 
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.use(deserializeUser)
 app.use(router)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import UserRouter from './user.route'
 import AuthRouter from './auth.route'
+import requireAuth from '../middelware/requireAuth'
 
 const router = express.Router()
 
-router.get('/test', (_, res) => res.sendStatus(200))
+router.get('/', requireAuth, (_, res) => res.sendStatus(200))
 
 router.use(UserRouter)
 router.use(AuthRouter)
